053639e90f28f0c2e619a4e184139ccd07a9ea51
Marcos Lima <marcoslima749@gmail.com>
2023-03-23 19:02:33 -0300

    comentado y agregado tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,74 +2,88 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 
+// Este archivo contiene un componente React que muestra varios cronómetros
+// y permite a los usuarios iniciar, detener y restablecer cada cronómetro.
+// Está pensado para hacer time tracking, por lo que al iniciar un cronómetro,
+// los demás se detienen.
+// Los usuarios también pueden cambiar el nombre de cada cronómetro.
+
+
 
 function App() {
 
-
-  const [timers, setTimers] = useState([
+  // Estado que almacena la información de cada cronómetro: su ID, tiempo y si está activo.
+  const [chron, setChron] = useState([
     { id: 1, time: new Date(2023, 0, 1, 0, 0, 0, 0), isActive: false },
     { id: 2, time: new Date(2023, 0, 1, 0, 0, 0, 0), isActive: false },
     { id: 3, time: new Date(2023, 0, 1, 0, 0, 0, 0), isActive: false },
   ]);
 
-  const [timerNames, setTimerNames] = useState([
+  // Estado que almacena el nombre de cada cronómetro.
+  const [crhonNames, setChronNames] = useState([
     "Timer 1",
     "Timer 2",
     "Timer 3",
   ]);
 
-  const handleNameChange = (timerId, newName) => {
-    setTimerNames((prevTimerNames) =>
-      prevTimerNames.map((name, index) =>
-        index === timerId - 1 ? newName : name
+  // Maneja el cambio de nombre de un cronómetro.
+  const handleNameChange = (chronId, newName) => {
+    setChronNames((prevChronNames) =>
+      prevChronNames.map((name, index) =>
+        index === chronId - 1 ? newName : name
       )
     );
   };
 
-  const startTimer = (timerId) => {
-    setTimers((prevTimers) =>
-      prevTimers.map((timer) =>
-        timer.id === timerId
-          ? { ...timer, isActive: true }
-          : { ...timer, isActive: false }
+  // Inicia un cronómetro con el ID dado y detiene todos los demás cronómetros.
+  const startChron = (chronId) => {
+    setChron((chronTimers) =>
+      chronTimers.map((chron) =>
+        chron.id === chronId
+          ? { ...chron, isActive: true }
+          : { ...chron, isActive: false }
       )
     );
   };
 
-  const stopTimer = (timerId) => {
-    setTimers((prevTimers) =>
-      prevTimers.map((timer) =>
-        timer.id === timerId ? { ...timer, isActive: false } : timer
+  // Detiene un cronómetro con el ID dado.
+  const stopChron = (chronId) => {
+    setChron((prevChrons) =>
+      prevChrons.map((chron) =>
+        chron.id === chronId ? { ...chron, isActive: false } : chron
       )
     );
   };
 
-  const clearTimer = (timerId) => {
-    setTimers((prevTimers) =>
-      prevTimers.map((timer) =>
-        timer.id === timerId ? { ...timer, time : new Date(2023, 0, 1, 0, 0, 0, 0), isActive: false } : timer
+  // Restablece un cronómetro con el ID dado a su estado original.
+  const clearChron = (chronId) => {
+    setChron((prevChrons) =>
+      prevChrons.map((chron) =>
+        chron.id === chronId ? { ...chron, time : new Date(2023, 0, 1, 0, 0, 0, 0), isActive: false } : chron
       )
     );
   };
 
+  // Actualiza el estado del cronómetro para reflejar el tiempo transcurrido.
   useEffect(() => {
+    // El temporizador se actualiza cada 10 milisegundos.
     const intervalId = setInterval(() => {
-      setTimers((prevTimers) =>
-        prevTimers.map((timer) =>
-          timer.isActive
+      setChron((prevChrons) =>
+        prevChrons.map((chron) =>
+          chron.isActive
             ? {
-                ...timer,
+                ...chron,
                 time: new Date(
                   2023,
                   0,
                   0,
-                  timer.time.getHours(),
-                  timer.time.getMinutes(),
-                  timer.time.getSeconds(),
-                  timer.time.getMilliseconds() + 10
+                  chron.time.getHours(),
+                  chron.time.getMinutes(),
+                  chron.time.getSeconds(),
+                  chron.time.getMilliseconds() + 10
                 ),
               }
-            : { ...timer, isActive: false }
+            : { ...chron, isActive: false }
         )
       );
     }, 10);
@@ -78,29 +92,29 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      {timers.map((timer,index) => (
-        <div key={timer.id} className="timerContainer">
+    <div className="container mx-auto">
+      {chron.map((chron,index) => (
+        <div key={chron.id} className="timerContainer align-middle flex flex-col">
           <h2>
             <input
-              className="inputName"
+              className="inputName text-2xl text-center align-middle"
               type="text"
-              value={timerNames[index]}
+              value={crhonNames[index]}
               onChange={(event) =>
-                handleNameChange(timer.id, event.target.value)
+                handleNameChange(chron.id, event.target.value)
               }
             />
           </h2>
-          <div>
-            Time:{" "}
-            {`${timer.time.toTimeString().substring(0, 8)},${timer.time
+          <div className="text-2xl text-center align-middle">
+            Time: 
+            {` ${chron.time.toTimeString().substring(0, 8)},${chron.time
               .getMilliseconds()
               .toString()
               .substring(0, 2)}`}
           </div>
-          <button className="button" onClick={() => startTimer(timer.id)}>Start</button>
-          <button className="button" onClick={() => stopTimer(timer.id)}>Stop</button>
-          <button className="button" onClick={() => clearTimer(timer.id)}>Clear</button>
+          <button className="bg-blue-400 hover:bg-blue-600" onClick={() => startChron(chron.id)}>Start</button>
+          <button className="button" onClick={() => stopChron(chron.id)}>Stop</button>
+          <button className="button" onClick={() => clearChron(chron.id)}>Clear</button>
         </div>
       ))}
     </div>
